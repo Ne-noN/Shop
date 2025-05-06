5f0872b86dce67f168dd7e49f222450cb70327d4
Ne-noN <https://github.com/Ne-noN/Shop>
2025-05-06 18:03:57 +0400

    второй коммит

--- django11/django11/settings.py
+++ django11/django11/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
-    BASE_DIR /'start_django/static',
+    os.path.join(BASE_DIR, 'start_django/static\css'),
 ]
 
 # Default primary key field type
